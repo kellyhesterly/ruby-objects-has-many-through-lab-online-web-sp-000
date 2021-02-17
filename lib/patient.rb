e41651a9701f83a4cb0e1bd2169a30c1fe53971e
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-17 04:34:49 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -18,6 +18,7 @@ class Patient
     Appointment.new(date, self, doctor)
   end
 
-  def doctors
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
   end
 end
